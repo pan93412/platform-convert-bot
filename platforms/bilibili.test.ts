6b09dc649aa60a7a2d5317ac07cd4dc29aadde27
Yi-Jyun Pan <me@pan93.com>
2025-03-17 17:50:11 +0800

    refactor(platforms/bilibili): Implement new platform

--- platforms/bilibili.test.ts
+++ platforms/bilibili.test.ts
@@ -2,38 +2,55 @@ import { describe, it, expect } from "vitest";
 import { bilibili } from "./bilibili";
 
 describe("Bilibili", () => {
-    it("should convert bilibili standard url to vxb23 url", () => {
-        const url = "https://www.bilibili.com/video/BV1oWQyYdECQ/";
-        const result = bilibili.convert(url);
-        expect(result).toBe("https://vxb23.tv/BV1oWQyYdECQ");
+    describe("shouldHandle", () => {
+        it("should return true if the url is a bilibili standard url", () => {
+            const url = new URL("https://www.bilibili.com/video/BV1oWQyYdECQ/");
+            const result = bilibili.shouldHandle(url);
+            expect(result).toBe(true);
+        });
+
+        it("should return true if the url is a bilibili short url", () => {
+            const url = new URL("https://b23.tv/nHaHjyu");
+            const result = bilibili.shouldHandle(url);
+            expect(result).toBe(true);
+        });
+
+        it("should return false if the url has been converted", () => {
+            const url = new URL("https://vxb23.tv/BV1oWQyYdECQ");
+            const result = bilibili.shouldHandle(url);
+            expect(result).toBe(false);
+        });
+
+        it("should return false if the url is not a bilibili url", () => {
+            const url = new URL("https://www.google.com");
+            const result = bilibili.shouldHandle(url);
+            expect(result).toBe(false);
+        });
     });
 
-    it("should convert bilibili short url to vxb23 url", () => {
-        const url = "https://b23.tv/nHaHjyu";
-        const result = bilibili.convert(url);
-        expect(result).toBe("https://vxb23.tv/nHaHjyu");
-    });
-
-    it("should convert bilibili short url with track id to vxb23 url", () => {
-        const url = "https://b23.tv/nHaHjyu?spm_id_from=333.337.search-card.all.click&vd_source=114514";
-        const result = bilibili.convert(url);
-        expect(result).toBe("https://vxb23.tv/nHaHjyu");
-    });
-
-    it("should convert bilibili short url with track id to vxb23 url", () => {
-        const url = "https://b23.tv/nHaHjyu?spm_id_from=333.337.search-card.all.click&vd_source=114514";
-        const result = bilibili.convert(url);
-        expect(result).toBe("https://vxb23.tv/nHaHjyu");
-    });
+    describe("convert", () => {
+        it("should convert bilibili standard url to vxb23 url", () => {
+            const url = new URL("https://www.bilibili.com/video/BV1oWQyYdECQ/");
+            const result = bilibili.convert(url);
+            expect(result).toBe("https://vxb23.tv/BV1oWQyYdECQ");
+        });
     
-    it("should leave the url unchanged if it's already a vxb23 url", () => {
-        const url = "https://vxb23.tv/nHaHjyu";
-        const result = bilibili.convert(url);
-        expect(result).toBe(url);
-    });
+        it("should convert bilibili short url to vxb23 url", () => {
+            const url = new URL("https://b23.tv/nHaHjyu");
+            const result = bilibili.convert(url);
+            expect(result).toBe("https://vxb23.tv/nHaHjyu");
+        });
 
-    it("should throw an error if the url is not a bilibili url", () => {
-        const url = "https://www.google.com";
-        expect(() => bilibili.convert(url)).toThrow("Invalid Bilibili URL: invalid hostname.");
+        it("should convert bilibili short url with track id to vxb23 url", () => {
+            const url = new URL("https://b23.tv/nHaHjyu?spm_id_from=333.337.search-card.all.click&vd_source=114514");
+            const result = bilibili.convert(url);
+            expect(result).toBe("https://vxb23.tv/nHaHjyu");
+        });
+    
+        it("should convert bilibili short url with track id to vxb23 url", () => {
+            const url = new URL("https://b23.tv/nHaHjyu?spm_id_from=333.337.search-card.all.click&vd_source=114514");
+            const result = bilibili.convert(url);
+            expect(result).toBe("https://vxb23.tv/nHaHjyu");
+        });
     });
 });
--- platforms/bilibili.ts
+++ platforms/bilibili.ts
@@ -4,20 +4,13 @@ export const bilibili: Platform = {
     command: "b23",
     name: "Bilibili",
     description: "將 Bilibili 的連結轉換成 vxb23 的連結",
-    convert(url: string): string {
-        const parsedUrl = new URL(url);
 
-        // 如果已經是 vxb23 的連結，則直接回傳（不處理）
-        if (parsedUrl.hostname === "vxb23.tv") {
-            return url;
-        }
+    shouldHandle(url: URL): boolean {
+        return url.hostname === "www.bilibili.com" || url.hostname === "b23.tv";
+    },
 
-        // 如果 hostname 不是 bilibili 或 b23，則拋出錯誤
-        if (parsedUrl.hostname !== "www.bilibili.com" && parsedUrl.hostname !== "b23.tv") {
-            throw new Error("Invalid Bilibili URL: invalid hostname.");
-        }
-
-        const pathname = parsedUrl.pathname.replace(/\/$/, "");
+    convert(url: URL): string {
+        const pathname = url.pathname.replace(/\/$/, "");
         const videoId = pathname.split("/").pop()?.trim();
         if (!videoId) {
             throw new Error("Invalid Bilibili URL: no video id found.");
