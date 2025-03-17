c87f7d3476fccf87917e3e41715a16a64747526e
Yi-Jyun Pan <me@pan93.com>
2025-03-17 17:49:59 +0800

    refactor(platforms): Auto convert

--- platforms/types.ts
+++ platforms/types.ts
@@ -13,10 +13,18 @@ export interface Platform {
     description: string;
 
     /**
+     * 決定是否要處理這則訊息。
+     * 
+     * @param url 網址
+     * @returns 是否要由這個 platform 處理
+     */
+    shouldHandle(url: URL): boolean;
+
+    /**
      * 傳入一個網址，轉換成整理過的連結
      * 
      * @param url 網址
      * @returns 整理過的連結
      */
-    convert(url: string): string;
+    convert(url: URL): string;
 }
